3aa397a4cf795007ede9420435a5ae7113e130c6
Jiawei Zhao <jiz@sdu.dk>
2024-03-21 12:22:29 +0100

    Updated answer to Q1

--- assignment2/Q2.py
+++ assignment2/Q2.py
@@ -1,3 +1,5 @@
+#! /opt/homebrew/anaconda3/bin/python
+
 import gymnasium as gym
 import math
 import random
@@ -15,17 +17,42 @@ import torch.nn.functional as F
 """
 Author: Jiawei Zhao
 Date: 19.03.2024
-Assignment 2 of DM887 (Reinforcement learning)
+Question 2 and Question 3 of Assignment 2 of DM887 (Reinforcement learning)
 Lecturer: Melih Kandemir
 Requirements: 
 
 References
 1. https://pytorch.org/tutorials/intermediate/reinforcement_q_learning.html
+2. https://gymnasium.farama.org/environments/classic_control/
 """
 
 T = 500 # max time step per episode
 N = 1050 # number of total episodes
-N0 = 50 # number of episodes per phase
+N0 = 50 # number of episodes per phase, including the warm-up, the antoencoder update, and the LSTD update phase
+
+"""
+general setup of plotting and 
+"""
+plt.ion()
+# use GPU training if possible
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
+
+"""
+exploring the state dimensions and action spaces of each of the required gym envs
+"""
+ENVS = ("Acrobot-v1", "MountainCar-v0", "Pendulum-v1")
+def explore_envs():
+    for env_name in ENVS:
+        env = gym.make(env_name)
+        print(f"Number of observations: {env.observation_space.shape[0]}")
+        print(f"Action space: {env.action_space}")
+        if type(env.action_space).__name__ == 'Discrete':
+            print(f"Number of possible actions of {env_name}: {env.action_space.n}")
+        else:
+            print(f"Action space of {env_name} has to undergo discretization!")
+        env.close()
 
 class Autoencoder(nn.Module):
     def __init__(self, input_dim, encoding_dim):
@@ -57,15 +84,16 @@ class Autoencoder(nn.Module):
 class ReplayMemory(object):
 
     def __init__(self, capacity: int, batch_size: int):
-        self.transition = namedtuple('transition', ('state', 'action', 'reward', 'state_next'))
         self.batch_size = batch_size
-        self.memory = deque([], maxlen=capacity)
+        self.memory = deque([], maxlen=capacity) # storing only the states to save memory instead using the standard (s, a, r, s') version
 
-    def push(self, *args):
-        """Save a transition to the buffer"""
-        self.memory.append(self.transition(*args))
+    def push(self, state: tuple):
+        """Save a state to the buffer instead of a transition defined at standard DQN"""
+        self.memory.append(state)
 
     def sample(self):
+        if len(self.memory) < self.batch_size:
+            return []
         return random.sample(self.memory, self.batch_size)
 
     def __len__(self):
@@ -74,24 +102,31 @@ class ReplayMemory(object):
 
 class LSTD_DQL_learner():
 
-    def __init__(self, capacity: int, env_name: str, encoding_dim: int, batch_size: int=32, gamma: float=0.9, lambda_val=1e-3):
-        self.batch_size = batch_size
-        self.mem= ReplayMemory(T*N, batch_size)
-        self.gamma = gamma
+    def __init__(self, device:str, env_name: str, n_actions: int, encoding_dim: int, batch_size: int=32, gamma: float=0.9, lambda_val=1e-2, learning_rate = 1e-3):
+        # used for both parts of training
+        self.n_actions = n_actions
         self.env = gym.make(env_name)
-        self.state_dim = self.env.observation_space.shape[0]
         self.encoding_dim = encoding_dim
-        self.inv_A = torch.eye(encoding_dim) / lambda_val # torch.eye creates identity matrix by default
-        self.b = torch.zeros(encoding_dim) # Initialize b as a zero vector
-        self.autoencoder = Autoencoder(self.state_dim, encoding_dim)
-        self.autoencoder_optimizer = optim.Adam(self.autoencoder.parameters(), lr=1e-3)
+        # used for the LSTD
+        self.gamma = gamma
+        self.inv_A = [torch.eye(encoding_dim) / lambda_val for _ in range(n_actions)] # torch.eye creates identity matrix by default
+        self.b = [torch.zeros(encoding_dim) for _ in range(n_actions) ] # Initialize b as a zero vector
+        self.theta = torch.rand((n_actions, encoding_dim)).to(device) # creates a matrix of random numbers between 0 and 1 with the given shape
+        # used for the autoencoder
+        self.mem= ReplayMemory(T*N, batch_size)
+        self.batch_size = batch_size
+        self.features()
+        self.learning_rate = learning_rate
+        self.state_dim = self.env.observation_space.shape[0]
+        self.autoencoder = Autoencoder(self.state_dim, encoding_dim).to(device)
+        self.autoencoder_optimizer = optim.Adam(self.autoencoder.parameters(), lr=learning_rate)
 
 
     def extract_features(self, state):
         # Use the encoder part of the autoencoder to extract features without using gradient descent
         with torch.no_grad():
             phi_s = self.autoencoder.encode(state)
-        return phi_s
+        self.features = phi_s
     
 
     def update_LSTD_inv_A_and_b(self, s, s_next, r):
@@ -110,4 +145,20 @@ class LSTD_DQL_learner():
 
 
 
-    
+if __name__ == "__main__":
+    """
+    only for experimental purposes
+    """
+    #explore_envs()
+    device = torch.device("cuda:0") if torch.cuda.is_available() else "cpu" # CUDA doesn't work with the AMD GPUs of MacBook M1
+    if device == "cuda:0":
+        print("Training on the GPU")
+    else:
+        print("Training on the CPU")
+    # define the objects use for gym env training after exploring the envs
+    ENV = namedtuple('env', ('name', 'n_actions', 'encoding_dim'))
+    env1 = ENV('Acrobot-v1', 3, 12) # 6x2, 6 is the number of observations in the env
+    env2 = ENV('MountainCar-v0', 3, 4) # 2X2, 2 is the number of observations in the env
+    # discretize the continuous space [-2, 2] into 10 equal intervals
+    env3 = ENV('Pendulum-v1', 10, 6) # 3x2, 3 is the number of observations in the env
+    ENVS = [env1, env2, env3]
--- assignment2/Q3.py
+++ assignment2/Q3.py
@@ -1,13 +1,28 @@
+#! /opt/homebrew/anaconda3/bin/python
+
 import matplotlib.pyplot as plt
+import gymnasium as gym
 import torch
+from collections import namedtuple
 from Q2 import LSTD_DQL_learner
 
-"""
-Author: Jiawei Zhao
-Date: 19.03.2024
-Assignment 2 of DM887 (Reinforcement learning)
-Lecturer: Melih Kandemir
-"""
+ENV = namedtuple('env', ('name', 'n_actions', 'encoding_dim'))
+
+env1 = ENV('Acrobot-v1', 3, 12) # 6x2, 6 is the number of observations in the env
+env2 = ENV('MountainCar-v0', 3, 4) # 2X2, 2 is the number of observations in the env
+# discretize the continuous space [-2, 2] into 10 equal intervals
+env3 = ENV('Pendulum-v1', 10, 6) # 3x2, 3 is the number of observations in the env
+
+ENVS = [env1, env2, env3]
 
 # use GPU for training if possible
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+if __name__ == "__main__":
+    # turn on interactive mode of pyplot
+    plt.ion()
+    # use GPU training if possible
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    if device == "cuda:0":
+        print("Running on the GPU")
+    # create the learner object
+    learner = LSTD_DQL_learner()
+    
