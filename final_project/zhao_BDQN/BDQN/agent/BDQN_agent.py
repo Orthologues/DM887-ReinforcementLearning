Fixed a bug of tensor normalization

final_project/zhao_BDQN/BDQN/agent/BDQN_agent.py
@@ -265,7 +265,7 @@ class BDQNAgent:
     @rval desired Tensor.shape = (4, 84, 84)
     """
     def init_episodal_states(self, x) -> torch.Tensor:
-        x = tensor(x).to(self.config.device).detach().permute(2, 0, 1)
+        x = tensor(x).to(device=self.config.device, dtype=torch.float32).detach().permute(2, 0, 1)
         x *= 1.0/255
         x = 0.2989 * x[0, :, :] + 0.5870 * x[1, :, :] + 0.1140 * x[2, :, :]
         # Add two dimensions to the start, the input frame has a shape at (1, 1, 210, 160) afterwards 

final_project/zhao_BDQN/BDQN/utils/normalizer.py
@@ -2,6 +2,8 @@ import numpy as np
 from torch import DeviceObjType, Tensor, nn, tensor, cat as concat_tensors
 from typing import Tuple, List, Union, Any, Dict
 
+import torch
+
 """
 The class to construct the 
 """
@@ -133,7 +135,7 @@ class AtariImageNormalizer(RescalingNormalizer):
             raise NotImplementedError("The Atari state frame must have a shape of (210, 160, 3)!")
 
         # $new_frame has a shape as (210, 160, 3), therefore, reshaping is necessary
-        x = tensor(new_frame).permute(2, 0, 1).detach().to(self.device)
+        x = tensor(new_frame).to(device=self.device, dtype=torch.float32).detach().permute(2, 0, 1)
         x = super().__call__(x)
         if self.rbg2greyscale:
             # Convert RGB to grayscale