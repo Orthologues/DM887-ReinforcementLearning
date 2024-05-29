0e2ddf656fdaf6b500ace2e3199349ca28daf68b
Jiawei Zhao <jiz@sdu.dk>
2024-05-29 09:02:45 +0200

    Revised the training parameters

--- final_project/zhao_BDQN/BDQN/utils/config.py
+++ final_project/zhao_BDQN/BDQN/utils/config.py
@@ -12,10 +12,10 @@ class Config:
     DEVICE = "cuda:0"
     """
     The frequency of a posterior update is 
-    1/(Config.TARGET_NETWORK_UPDATE_INTERVAL * Config.TARGET_WEIGHT_UPDATE_INTERVAL) = 1/10000
+    1/(Config.TARGET_NETWORK_UPDATE_INTERVAL * Config.TARGET_WEIGHT_UPDATE_INTERVAL) = 1/2000
     """
-    TARGET_NETWORK_UPDATE_INTERVAL = 2 * 10**3
-    TARGET_WEIGHT_UPDATE_INTERVAL = 5
+    TARGET_NETWORK_UPDATE_INTERVAL = 1 * 10**3
+    TARGET_WEIGHT_UPDATE_INTERVAL = 2
     GD_UPDATE_INTERVAL = 10 # gradient descent update frequency for the policy Q-network
     WARMUP_STEPS = 2 * 10**4
     THOMPSON_SAMPLING_INTERVAL = 10**3
@@ -25,7 +25,7 @@ class Config:
     STATE_HEIGHT = 84
     DEFAULT_OPTIMIZER_FN = lambda params: torch.optim.Adam(
     params, lr=2.5e-3, betas=(0.9, 0.999), eps=0.01) 
-    REPLAY_HISTORY_LENGTH = 4
+    REPLAY_HISTORY_LENGTH = 5
     REPLAY_BUFFER = lambda capacity: ReplayMemory(capacity)
     CONV_NETWORK = lambda input_dim: BdqnConvNet(input_dim)
     SIGMA_VARIANCE = 0.001
